98f10c0151f2e619f936deebe27b0956e1746943
GentlemanBATMAN <ltv.ihor@gmail.com>
2023-10-09 10:25:32 +0200

    Completed tutorial(ask GPT if something is missing)

--- Assets/Scripts/PalyerNetwork.cs
+++ Assets/Scripts/PalyerNetwork.cs
@@ -2,13 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Collections;
 
 public class PalyerNetwork : NetworkBehaviour
 {
+    [SerializeField] private Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform;
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData{_int = 56, _bool = true,},
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + "; Random number: " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+            //TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } });
+            //TestServerRPC(new ServerRpcParams());
+            //randomNumber.Value = new MyCustomData { _int = 10, _bool =  false, message = "All your bases belong to us" };
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            spawnedObjectTransform.GetComponent<NetworkObject>().Despawn(true); 
+            //Destroy(spawnedObjectPrefab.gameObject);
+        }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -19,5 +62,17 @@ public class PalyerNetwork : NetworkBehaviour
 
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRPC(ServerRpcParams serverRpcParams)
+    {
+        Debug.Log("TestServerRPC " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc]
+    private void TestClientRpc(ClientRpcParams clientRpcParams)
+    {
+        Debug.Log("TestClientRpc");
     }
 }
